0810218a39eeb01277a5dad45baabf55e40f8eb5
Thomas Smith <leanbrizko@gmail.com>
2025-03-09 14:42:26 -0400

    de black and gold

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// /js/script.js
+
 document.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
     const blurValue = Math.min(scrollPosition / 40, 5); // Adjust blur based on scroll position, max 5px
